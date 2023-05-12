47ac56c5dccec7c0d3c55908da1e7a6598cb6633
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-05-12 19:12:17 -0300

    Terceiro commit 12/05/23

--- src/components/layout/Dias.js
+++ src/components/layout/Dias.js
@@ -2,7 +2,6 @@ import styles from "./Dias.module.css";
 
 import minima_b from "../../img/minima_b.png";
 import maxima_b from "../../img/maxima_b.png";
-import tempestade_b from "../../img/tempestade_b.png";
 
 
 function Dias(props){
@@ -23,12 +22,12 @@ function Dias(props){
             </ul>
         </div>
         <div className={styles.tempo}>
-            <div>{props.desClima}</div>
+            {props.desClima}
             <span className={styles.ensolarado}>{props.clima}</span>
         </div>
         <div className={styles.temperatura}>
             <div className={styles.temperaturaCima}>
-                <img className={styles.desMinimo} src={minima_b} ></img>
+                {props.imgMin}
                 <span className={styles.min}>Min.</span>
                 <span className={styles.minNumero}>{props.min}</span>
             </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,10 @@ import styles from "./index.module.css";
 import Header from "../../components/layout/Header";
 import Dias from "../../components/layout/Dias";
 import ensolarado_b from "../../img/ensolarado_b.png";
+import tempestade_b from "../../img/tempestade_b.png";
+import chuvoso_b from "../../img/chuvoso_b.png";
+import nublado_b from "../../img/nublado_b.png";
+import minima_b from "../../img/minima_b.png";
 
 function Home(){
 
@@ -13,11 +17,51 @@ function Home(){
                     esquerda
                 </div>
                 <div className={styles.direita}>
-                    <Dias dia1="Sexta" dia2="11/10/2019" desClima={<img src="ensolarado_b"></img>} clima="Ensolarado" min="15°" max="25°"/>
-                    <Dias dia1="Sábado" dia2="12/10/2019" desClima="aa" clima="Ensolarado" min="18°" max="31°"/>
-                    <Dias dia1="Domingo" dia2="13/10/2019" desClima="aa" clima="Tempestade" min="13°" max="23°"/>
-                    <Dias dia1="Segunda" dia2="14/10/2019" desClima="aa" clima="Chuvoso" min="15°" max="24°"/>
-                    <Dias dia1="Terça" dia2="15/10/2019" desClima="aa" clima="Nublado" min="15°" max="24°"/>
+                    <Dias 
+                        dia1="Sexta" 
+                        dia2="11/10/2019" 
+                        desClima={<img className={styles.imgEnsolarado} src={ensolarado_b} ></img>} 
+                        imgMin={<img className={styles.desMinimo} src={minima_b}></img>}
+                        clima="Ensolarado"                         
+                        min="15°" 
+                        max="25°"
+                    />
+                    <Dias 
+                        dia1="Sábado" 
+                        dia2="12/10/2019" 
+                        desClima={<img className={styles.imgEnsolarado} src={ensolarado_b}></img>} 
+
+                        clima="Ensolarado"                        
+                        min="18°" 
+                        max="31°"
+                    />
+                    <Dias 
+                        dia1="Domingo" 
+                        dia2="13/10/2019" 
+                        desClima={<img className={styles.imgTempestade} src={tempestade_b}></img>} 
+
+                        clima="Tempestade"                         
+                        min="13°" 
+                        max="23°"
+                    />
+                    <Dias 
+                        dia1="Segunda" 
+                        dia2="14/10/2019" 
+                        desClima={<img className={styles.imgChuvoso} src={chuvoso_b}></img>} 
+
+                        clima="Chuvoso"                         
+                        min="15°" 
+                        max="24°"
+                    />
+                    <Dias 
+                        dia1="Terça" 
+                        dia2="15/10/2019" 
+                        desClima={<img className={styles.imgNublado} src={nublado_b}></img>} 
+
+                        clima="Nublado"                         
+                        min="15°" 
+                        max="24°"
+                    />
                 </div>
             </div>
 
