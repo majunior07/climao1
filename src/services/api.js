5418efcc3efea8507cf1bcd153648422d151d59e
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-05-23 19:51:05 -0300

    Segundo commit 23/05/23

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: '',
-});
+const api = "http://api.openweathermap.org/data/2.5/weather?q=campinas,uk&APPID=24aed0ce608584b26ff622cfcd55c765";
 
-export default api;
+function getClima() {
+    axios.get(api) 
+        .then(response => {
+            console.log(response)
+        })  
+        .catch(error => console.log(error))
+    }
+
+getClima();
+
+ 
