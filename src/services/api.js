73829ba9c635483251762d6bced290714092d140
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-05-23 15:34:58 -0300

    Primeiro commit 23/05/23

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: '',
-});
+const api = "http://www.weatherapi.com";
+
+function getClima(){
+    axios.get(api)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error=> console.log(error))
+}
+
+getClima();
 
 export default api;
