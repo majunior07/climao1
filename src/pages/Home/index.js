6174b03e105e98df2b114371d767b84f321d5305
Maurício Augusto Júnior <ma.juniorkf@hotmail.com>
2023-06-05 15:02:42 -0300

    Segundo commit 05/06/2023

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -107,10 +107,12 @@ function Home(){
                     </div>
                 }
 
-                { clima.forecast != undefined &&
+                
 
-                    <div className={styles.direita}>                  
-                     
+                    <div className={styles.direita}>         
+
+                    { clima.forecast != undefined &&  
+
                         <Dias setClima={setClima}
                             dia1="Sexta" 
                             dia2={clima.forecast.forecastday[1].date} 
@@ -120,62 +122,31 @@ function Home(){
                             min={`${clima.forecast.forecastday[1].day.mintemp_c.toFixed()} º`} 
                             imgMax={<img src={maxima_b}></img>}
                             max={`${clima.forecast.forecastday[1].day.maxtemp_c.toFixed()} º`}
-                        />
-                   
+                        />                
+                    }
 
-                     
-                        <Dias 
-                            dia1="Sábado" 
+                    { clima.forecast != undefined &&  
+
+                        <Dias setClima={setClima}
+                            dia1="Sabado" 
                             dia2={clima.forecast.forecastday[2].date} 
-                            imgClima={<img className={styles.imgEnsolarado} src={clima.forecast.forecastday[2].day.condition.icon}></img>} 
+                            imgClima={<img className={styles.imgEnsolarado} src={clima.forecast.forecastday[2].day.condition.icon} ></img>} 
                             clima={clima.forecast.forecastday[2].day.condition.text}
-                            imgMin={<img src={minima_b}></img>}                      
+                            imgMin={<img src={minima_b}></img>}                         
                             min={`${clima.forecast.forecastday[2].day.mintemp_c.toFixed()} º`} 
                             imgMax={<img src={maxima_b}></img>}
                             max={`${clima.forecast.forecastday[2].day.maxtemp_c.toFixed()} º`}
-                        />
-                    
+                        />                
+                    }
 
-                    
-                        <Dias 
-                            dia1="Sábado" 
-                            dia2={clima.forecast.forecastday[3].date} 
-                            imgClima={<img className={styles.imgEnsolarado} src={clima.forecast.forecastday[3].day.condition.icon}></img>} 
-                            clima={clima.forecast.forecastday[3].day.condition.text}
-                            imgMin={<img src={minima_b}></img>}                      
-                            min={`${clima.forecast.forecastday[3].day.mintemp_c.toFixed()} º`} 
-                            imgMax={<img src={maxima_b}></img>}
-                            max={`${clima.forecast.forecastday[3].day.maxtemp_c.toFixed()} º`}
-                        />
-                   
+                                 
+                
 
-                    
-                        <Dias 
-                            dia1="Sábado" 
-                            dia2={clima.forecast.forecastday[4].date} 
-                            imgClima={<img className={styles.imgEnsolarado} src={clima.forecast.forecastday[4].day.condition.icon}></img>} 
-                            clima={clima.forecast.forecastday[4].day.condition.text}
-                            imgMin={<img src={minima_b}></img>}                      
-                            min={`${clima.forecast.forecastday[4].day.mintemp_c.toFixed()} º`} 
-                            imgMax={<img src={maxima_b}></img>}
-                            max={`${clima.forecast.forecastday[4].day.maxtemp_c.toFixed()} º`}
-                        />
                   
 
-                    
-                        <Dias 
-                            dia1="Sábado" 
-                            dia2={clima.forecast.forecastday[5].date} 
-                            imgClima={<img className={styles.imgEnsolarado} src={clima.forecast.forecastday[5].day.condition.icon}></img>} 
-                            clima={clima.forecast.forecastday[5].day.condition.text}
-                            imgMin={<img src={minima_b}></img>}                      
-                            min={`${clima.forecast.forecastday[5].day.mintemp_c.toFixed()} º`} 
-                            imgMax={<img src={maxima_b}></img>}
-                            max={`${clima.forecast.forecastday[5].day.maxtemp_c.toFixed()} º`}
-                        />
                    
                     </div>
-                }
+                
 
             </div>
 
