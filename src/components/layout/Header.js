380978b3e60bf69defe458394d0e010607c3216e
Mauricio Augusto Junior | Hospital Unimed Poços de Caldas <email@email.com>
2023-06-16 16:14:49 -0300

    Terceiro commit 16/06/23

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,20 +5,21 @@ import axios from "axios";
 import styles from "./Header.module.css";
 import logo from "../../img/LOGO.png";
 import Busca from "../../img/Busca.png";
+import api from "../../services/api";
 
 function Header({setClima}){
 
     const [city, setCity] = useState('Campinas');
     
 
-    const url = `http://api.weatherapi.com/v1/forecast.json?key=6dc397328dea4991b1e175154232205&q=Campinas&days=6&aqi=no&alerts=no&lang=pt&q=${city}`;
+    //const url = `http://api.weatherapi.com/v1/forecast.json?key=6dc397328dea4991b1e175154232205&days=6&aqi=no&alerts=no&lang=pt&q=${city}`;
 
     const handleChange = (e) => {
         setCity(e.target.value)
     }
 
     function handleSearch() {
-        axios.get(url)
+        axios.get()
         .then((response) => {
             setClima(response.data);
             console.log(response.data)
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,18 @@
-const api = "";
+import axios from "axios";
+
+//Criar uma instância do Axios com a baseURL
+const api = axios.create({
+    baseURL: 'http://api.weatherapi.com/v1/forecast.json?key=6dc397328dea4991b1e175154232205&days=6&aqi=no&alerts=no&lang=pt&q=Campinas',
+});
+/*
+//Adicionar um interceptor para todas as requisições
+api.interceptors.request.use((config) => {
+    // Adicionar a chave de API á URL
+    config.url += `?key=6dc397328dea4991b1e175154232205&days=3&aqi=no&alerts=no&lang=pt`;
+
+    return config;
+});
+*/
 
 export default api;
 
